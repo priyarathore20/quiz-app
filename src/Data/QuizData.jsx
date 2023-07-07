18dc9e870b13d09af67b085f168078d8cdbe9f13
priya rathore <rathorepriya1705@gmail.com>
2023-07-07 23:12:35 +0530

    initial commit

---
+++ src/Data/QuizData.jsx
@@ -0,0 +1,52 @@
+export const QuizData = [
+  {
+      question: "Which language runs in a web browser?",
+      options:[ "Java", "C", "Python", "JavaScript"],
+      answer: 4
+  },
+  {
+      question: "What does CSS stand for?",
+      options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+      answer: 2
+  },
+  {
+      question: "What does HTML stand for?",
+      options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+      answer: 1
+  },
+  {
+      question: "What year was JavaScript launched?",
+      options: ["1996", "1995", "1994", "none of the above"],
+      answer:2
+  },
+  {
+    question: "What is the correct tag for a line break?",
+    options: ["brk /", "line /", "bk /", "br /"],
+    answer:4
+},
+{
+    question: "What is the correct format for aligning written content to the center of the page in CSS?",
+    options: ["Text-align:center;", "Font-align:center;", "Text:align-center;", "none of the above"],
+    answer:1
+},
+{
+    question: "A domain name is a identification label.",
+    options: ["True", "False"],
+    answer:1
+},
+{
+    question: "Which property is used to change the background color?",
+    options: ["background-color", "color", 'bgColor', "None of the above."],
+    answer:1
+},
+{
+    question: "jQuery uses CSS selectors to select elements.",
+    options: ["True", "False"],
+    answer:1
+},
+{
+    question: "Which jQuery method is used to hide selected elements?",
+    options: ["hidden()", "visible(false)", "hide()", "display(none)"],
+    answer:3
+},
+];
